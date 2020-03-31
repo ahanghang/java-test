1ced36124cad2e40c1b393f023c3740297d74251
hang <zouxh_java@163.com>
2020-03-31 17:11:13 +0800

    添加test2类

---
+++ src/com/test/Test2.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author zxh
+ * @create 2020-03-31 17:10
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println(".....test2");
+    }
+}
