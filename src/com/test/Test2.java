5376de5dc921ab9e5d6979fad6d06f54da326022
hang <zouxh_java@163.com>
2020-03-31 17:41:00 +0800

    java-test-git-test.....Test2类添加内容

--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -8,5 +8,8 @@ public class Test2 {
 
     public static void main(String[] args) {
         System.out.println(".....test2");
+
+        
+        System.out.println(".....java-test-git-test.......main......1");
     }
 }
