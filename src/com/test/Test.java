58bbe2bac205e4f8ede1c2021ab70101f916c0ef
hang <zouxh_java@163.com>
2020-03-31 17:13:29 +0800

    test类添加内容

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -8,5 +8,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("...............test");
+        System.out.println(".............test java-test");
+        System.out.println(".............test java-test 1");
+        System.out.println(".............test java-test 2");
     }
 }
