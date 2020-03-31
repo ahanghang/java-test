7436087c3c8e8f2e4b256ea13a0aef69d5205b9e
hang <zouxh_java@163.com>
2020-03-31 17:07:25 +0800

    添加test类

---
+++ src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author zxh
+ * @create 2020-03-31 17:06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("...............test");
+    }
+}
