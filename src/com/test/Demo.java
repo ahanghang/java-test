3befcb75c7e508926a7d3aa6dea922ef2a370927
hang <zouxh_java@163.com>
2020-03-31 17:01:18 +0800

    添加demo类

---
+++ src/com/test/Demo.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author zxh
+ * @create 2020-03-31 17:00
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("demo main");
+    }
+}
