183cadb4778e7ddbcf16229c2143ce43c526070f
hang <zouxh_java@163.com>
2020-04-01 09:20:43 +0800

    添加Test3类

---
+++ src/com/test/Test3.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author zxh
+ * @create 2020-04-01 9:19
+ */
+public class Test3 {
+
+    public static void main(String[] args) {
+        System.out.println("............test main()");
+    }
+}
